1ebf86ce500ac3f7ad4126cdd137ba415526116e
IMPixel <asemenov.ca@gmail.com>
2022-07-17 19:23:46 -0400

    allowed switching from followers to following

--- public/scripts/followersFollowing.js
+++ public/scripts/followersFollowing.js
@@ -8,8 +8,8 @@ let followersFollowingTopUsernameLabel = document.getElementById("followersFollo
 
 let currentFollowersList = null
 let currentFollowingList = null
-let followersListCached = false
-let followingListCached = false
+let followersListRendered = false
+let followingListRendered = false
 let currentOnClose = function() {}
 
 async function setFollowersFollowing(isFollowers, username, followersList, followingList, onClose) {
@@ -20,8 +20,8 @@ async function setFollowersFollowing(isFollowers, username, followersList, follo
 
     currentFollowersList = followersList
     currentFollowingList = followingList
-    followersListCached = false
-    followingListCached = false
+    followersListRendered = false
+    followingListRendered = false
 
     if(isFollowers) {
         followersSwitch.style.color = "var(--theme-color)"
@@ -29,44 +29,71 @@ async function setFollowersFollowing(isFollowers, username, followersList, follo
         followersContainer.style.display = "block"
         followingContainer.style.display = "none"
         
-        await cacheBasicUserDataFromArray(followersList)
-        followersListCached = true
-
-        // updating dom
-        for(let i = 0; i != followersList.length; i++) {
-            let userData = getCachedBasicUserData(followersList[i])
-            let element = document.createElement("followers-list-element")
-            element.setAttribute("username", userData.username)
-            element.setAttribute("profile-picture", userData.profilePicture)
-            element.setAttribute("bio", userData.bio)
-            element.setAttribute("id", userData.id)
-            followersContainer.appendChild(element)
-        }
+        await renderFollowersList(followersList)
     } else {
         followersSwitch.style.color = "#d1d1d1"
         followingSwitch.style.color = "var(--theme-color)"
         followersContainer.style.display = "none"
         followingContainer.style.display = "block"
 
-        console.log(followingList)
-        await cacheBasicUserDataFromArray(followingList)
-        followingListCached = true
-
-        // updating dom
-        for(let i = 0; i != followingList.length; i++) {
-            let userData = getCachedBasicUserData(followingList[i])
-            let element = document.createElement("followers-list-element")
-            element.setAttribute("username", userData.username)
-            element.setAttribute("profile-picture", userData.profilePicture)
-            element.setAttribute("bio", userData.bio)
-            element.setAttribute("id", userData.id)
-            followingContainer.appendChild(element)
-        }
+        await renderFollowingList(followingList)
     }
     followersFollowingTopUsernameLabel.textContent = username
+}
+
+async function renderFollowersList(followersList) {
+    await cacheBasicUserDataFromArray(followersList)
+    followersListRendered = true
+
+    // updating dom
+    for(let i = 0; i != followersList.length; i++) {
+        let userData = getCachedBasicUserData(followersList[i])
+        let element = document.createElement("followers-list-element")
+        element.setAttribute("username", userData.username)
+        element.setAttribute("profile-picture", userData.profilePicture)
+        element.setAttribute("bio", userData.bio)
+        element.setAttribute("id", userData.id)
+        followersContainer.appendChild(element)
+    }
+}
+
+async function renderFollowingList(followingList) {
+    await cacheBasicUserDataFromArray(followingList)
+    followingListRendered = true
+
+    // updating dom
+    for(let i = 0; i != followingList.length; i++) {
+        let userData = getCachedBasicUserData(followingList[i])
+        let element = document.createElement("followers-list-element")
+        element.setAttribute("username", userData.username)
+        element.setAttribute("profile-picture", userData.profilePicture)
+        element.setAttribute("bio", userData.bio)
+        element.setAttribute("id", userData.id)
+        followingContainer.appendChild(element)
+    }
 }
 
 followersFollowingTopBackButton.addEventListener("click", (event) => {
     followersFollowingContainer.style.display = "none"
     currentOnClose()
+})
+
+followersSwitch.addEventListener("click", (event) => {
+    followersSwitch.style.color = "var(--theme-color)"
+    followingSwitch.style.color = "#d1d1d1"
+    followersContainer.style.display = "block"
+    followingContainer.style.display = "none"
+    if(!followersListRendered) {
+        renderFollowersList(currentFollowersList)
+    }
+})
+
+followingSwitch.addEventListener("click", (event) => {
+    followersSwitch.style.color = "#d1d1d1"
+    followingSwitch.style.color = "var(--theme-color)"
+    followersContainer.style.display = "none"
+    followingContainer.style.display = "block"
+    if(!followingListRendered) {
+        renderFollowingList(currentFollowingList)
+    }
 })
